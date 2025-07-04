725b742299937656bc9905ab8530dbf99694ef82
Juan Ignacio Londra <londrajuan@gmail.com>
2025-07-04 15:26:32 -0300

    feature: add student comments

--- src/app/student/student.ts
+++ src/app/student/student.ts
@@ -8,6 +8,7 @@ import { Component, Input } from '@angular/core';
   styleUrl: './student.scss'
 })
 export class Student {
+  //con el decorador @Input() se reciben datos desde el componente padre
   @Input() student!: { name: string; age: number; birthDate: Date  }; // Definimos la interfaz del estudiante
 
 }
